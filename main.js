561e9dd02da3dc62314ebc647736d486a17f24cc
WIPocket <kolnika@ddmp9.local>
2019-03-07 18:22:52 +0100

    Aby si Codacy nestezoval

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const fetchInterval = 25000;
 const dataUrl = "http://jimmyscgm.nightscout.cz/pebble";
 
+const dateFormatter = new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 });
+
 const arrows = {
   DoubleDown    : "&#x21CA;",
   SingleDown    : "&#x2193;",
@@ -8,24 +10,24 @@ const arrows = {
   Flat          : "&#x2192;",
   FortyFiveUp   : "&#x2197;",
   SingleUp      : "&#x2191;",
-  DoubleUp      : "&#x21C8;",
+  DoubleUp      : "&#x21C8;"
 };
 
-addEventListener("load",function() {
+addEventListener("load", function() {
   FetchData();
 });
 
 function Update(json) {
   json = JSON.parse(json);
-  let localDate = new Date();
+  const localDate = new Date();
 
-  let lastUpDate = new Date(json.status[0].now - json.bgs[0].datetime);
+  const lastUpDate = new Date(json.status[0].now - json.bgs[0].datetime);
 
-  let data = {
+  const data = {
     lastUpdate: lastUpDate.getMinutes() ? lastUpDate.getMinutes() : lastUpDate.getSeconds(),
     lastUpdateInSec: !lastUpDate.getMinutes(),
 
-    time: localDate.getHours().pad(2) + " : " + localDate.getMinutes().pad(2),
+    time: dateFormatter.format(localDate.getHours()) + " : " + dateFormatter.format(localDate.getMinutes()),
 
     delta: json.bgs[0].bgdelta,
 
@@ -33,13 +35,13 @@ function Update(json) {
 
     glucose: json.bgs[0].sgv,
 
-    trendArrow: arrows[json.bgs[0].direction],
+    trendArrow: arrows[json.bgs[0].direction]
   };
 
   WriteToScreen(data);
 }
 
-function WriteToScreen(data){
+function WriteToScreen(data) {
   console.log(data);
 
   document.getElementById("time")      .innerText = data.time;
@@ -52,24 +54,18 @@ function WriteToScreen(data){
 function FetchData() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-     if (xmlhttp.status == 200) {
-       Update(xmlhttp.responseText);
-       setTimeout(FetchData, fetchInterval);
-     } else {
-       setTimeout(FetchData, fetchInterval * 2);
-     }
+    if(xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
+      if(xmlhttp.status == 200) {
+        Update(xmlhttp.responseText);
+        setTimeout(FetchData, fetchInterval);
+      } else {
+        setTimeout(FetchData, fetchInterval * 2);
+      }
     }
   };
-  xmlhttp.onerror = function(){
+  xmlhttp.onerror = function() {
     setTimeout(FetchData, fetchInterval * 2);
   };
   xmlhttp.open("GET", dataUrl, true);
   xmlhttp.send();
 }
-
-Number.prototype.pad = function(width, z) {
-  z = z || '0';
-  let n = this.toString();
-  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-};
