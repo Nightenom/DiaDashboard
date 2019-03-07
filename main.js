2fff5b0b0e2d229d71946cb35c9e3eb65bc0bc21
WIPocket <kolnika@ddmp9.local>
2019-03-07 18:54:01 +0100

    just checking if git is working properly...

--- main.js
+++ main.js
@@ -1,71 +1,73 @@
-    const fetchInterval = 25000;
-    const dataUrl = "http://jimmyscgm.nightscout.cz/pebble";
+const fetchInterval = 25000;
+const dataUrl = "http://jimmyscgm.nightscout.cz/pebble";
 
-    const dateFormatter = new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 });
+const dateFormatter = new Intl.NumberFormat('en-IN', {
+    minimumIntegerDigits: 2
+});
 
-    const arrows = {
-    DoubleDown    : "&#x21CA;",
-    SingleDown    : "&#x2193;",
-    FortyFiveDown : "&#x2198;",
-    Flat          : "&#x2192;",
-    FortyFiveUp   : "&#x2197;",
-    SingleUp      : "&#x2191;",
-    DoubleUp      : "&#x21C8;"
-    };
+const arrows = {
+    DoubleDown:    "&#x21CA;",
+    SingleDown:    "&#x2193;",
+    FortyFiveDown: "&#x2198;",
+    Flat:          "&#x2192;",
+    FortyFiveUp:   "&#x2197;",
+    SingleUp:      "&#x2191;",
+    DoubleUp:      "&#x21C8;"
+};
 
-    addEventListener("load", function() {
-    FetchData();
-    });
+addEventListener("load", function() {
+    fetchData();
+});
 
-    function Update(json) {
+function update(json) {
     json = JSON.parse(json);
     const localDate = new Date();
 
     const lastUpDate = new Date(json.status[0].now - json.bgs[0].datetime);
 
     const data = {
-    lastUpdate: lastUpDate.getMinutes() ? lastUpDate.getMinutes() : lastUpDate.getSeconds(),
-    lastUpdateInSec: !lastUpDate.getMinutes(),
+        lastUpdate: lastUpDate.getMinutes() ? lastUpDate.getMinutes() : lastUpDate.getSeconds(),
+        lastUpdateInSec: !lastUpDate.getMinutes(),
 
-    time: dateFormatter.format(localDate.getHours()) + " : " + dateFormatter.format(localDate.getMinutes()),
+        time: dateFormatter.format(localDate.getHours()) + " : " + dateFormatter.format(localDate.getMinutes()),
 
-    delta: json.bgs[0].bgdelta,
+        delta: json.bgs[0].bgdelta,
 
-    battery: json.bgs[0].battery,
+        battery: json.bgs[0].battery,
 
-    glucose: json.bgs[0].sgv,
+        glucose: json.bgs[0].sgv,
 
-    trendArrow: arrows[json.bgs[0].direction]
+        trendArrow: arrows[json.bgs[0].direction]
     };
 
-    WriteToScreen(data);
-    }
+    writeToScreen(data);
+}
 
-    function WriteToScreen(data) {
+function writeToScreen(data) {
     console.log(data);
 
-    document.getElementById("time")      .innerText = data.time;
+    document.getElementById("time").innerText = data.time;
     document.getElementById("lastUpdate").innerText = data.lastUpdate + (data.lastUpdateInSec ? " sec" : " min");
-    document.getElementById("glucose")   .innerText = data.glucose;
+    document.getElementById("glucose").innerText = data.glucose;
     document.getElementById("trendArrow").innerHTML = data.trendArrow;
-    document.getElementById("delta")     .innerText = data.delta;
-    }
+    document.getElementById("delta").innerText = data.delta;
+}
 
-    function FetchData() {
+function fetchData() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-    if(xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
-    if(xmlhttp.status == 200) {
-    Update(xmlhttp.responseText);
-    setTimeout(FetchData, fetchInterval);
-    } else {
-    setTimeout(FetchData, fetchInterval * 2);
-    }
-    }
+        if(xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
+            if(xmlhttp.status == 200) {
+                update(xmlhttp.responseText);
+                setTimeout(fetchData, fetchInterval);
+            } else {
+                setTimeout(fetchData, fetchInterval * 2);
+            }
+        }
     };
     xmlhttp.onerror = function() {
-    setTimeout(FetchData, fetchInterval * 2);
+        setTimeout(fetchData, fetchInterval * 2);
     };
     xmlhttp.open("GET", dataUrl, true);
     xmlhttp.send();
-    }
+}
